0cb698320a85874442a43c5570324911d28d6f86
Viana <viana.jc@pg.com>
2026-03-16 06:50:11 -0500

    done

---
+++ 20260212/01_welcome.js
@@ -0,0 +1,8 @@
+
+let a = 10
+let b = 20
+let sum = a + b
+document.write("Welcome to external JS file");
+document.write("<br/>Sum of two number is " + sum)
+document.getElementById("p1").innerHTML="Hello"
+document.getElementById("p1").innerHTML="Welcome to JS" //advisable

34dcbb9404103a987a9840493f414887c7ef384b
Viana <viana.jc@pg.com>
2026-03-17 09:46:08 -0500

    done

--- 20260213/01_domoperations.js
+++ 20260213/01_domoperations.js
@@ -1,3 +1,18 @@
-function AddOperation(){
-    alert("Event fired")
+function addOperation(){
+    // alert("Event fired")
+    let divTag = document.createElement("div");
+    let nameValue = document.getElementById("name").value
+    let ageValue = document.getElementById("age").value
+    let divTagContent = document.createTextNode(`Name is ${nameValue} and age is ${ageValue}`)
+    divTag.appendChild(divTagContent)
+    if(ageValue>=40){
+        divTag.setAttribute("class","abc")
+    } else {
+        divTag.setAttribute("class","xyc")
+    }
+
+    document.getElementsByTagName("body")[0].appendChild(divTag)
+    // document.getElementById("result").appendChild(divTag)
+    document.getElementById("name").value=""
+    document.getElementById("age").value=""
 }
